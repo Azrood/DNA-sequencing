89c2ea4455255741958e4732898dea02925fb0b9
anas lhichou <hichouanas@gmail.com>
2021-06-13 15:09:56 +0100

    tests

--- src/pages/dna_analysis.py
+++ src/pages/dna_analysis.py
@@ -1,14 +1,23 @@
 from .dna_func import *
 from django.conf import settings
 from Bio import SeqIO
+from collections import Counter
+import plotly.graph_objects as go
+import plotly
+
+file = open(f'{settings.MEDIA_ROOT}/covid19.fasta','r')
+dna_file = SeqIO.parse(file,"fasta")
+l = [record for record in dna_file] 
 
 def analysis():
-    file = open(f'{settings.MEDIA_ROOT}/covid19.fasta','r')
-    dna_file = SeqIO.parse(file,"fasta")
-    #st.write(dna_record)
-    l = [record for record in dna_file]  
+     
     dna_record = l[0]
-    dna_seq= dna_record.seq
+    return dna_record
 
-
-    return dna_record.description
+def analysis_plot():
+    dna_freq = Counter(analysis().seq)
+    fig = go.Figure(
+    data=[go.Bar(x=list(dna_freq.keys()),y=list(dna_freq.values()))],
+    layout_title_text="test"
+    )
+    return fig
--- src/pages/views.py
+++ src/pages/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from pages.dna_analysis import analysis
 from pages.models import FilesUpload
 from .forms import FilesUploadFrom
-from .dna_analysis import analysis
+from .dna_analysis import analysis,analysis_plot
+from collections import Counter
+import plotly
 # Create your views here.
 def home_view(request,*args,**kwargs):
     return render(request,"index.html",{})
@@ -17,8 +19,9 @@ def analyse_view(request,*args,**kwargs):
             return render(request,"analyse.html",{'form': form})
     else:
         form = FilesUploadFrom()
+    print(Counter(analysis().seq))
 
-    return render(request,"analyse.html",{'form': form,'dna_desc':analysis()})
+    return render(request,"analyse.html",{'form': form,'dna_desc':analysis().description,'nuc_freq':dict(Counter(analysis().seq)),'graph_div':plotly.offline.plot(analysis_plot(), auto_open = False, output_type="div")})
 
 def vis3d_view(request,*args,**kwargs):
     return render(request,"3dvis.html",{})
