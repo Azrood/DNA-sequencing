e5bfcee9016fe52be9cf05b37a4e7a360534d1d5
Azrood <53349565+Azrood@users.noreply.github.com>
2021-06-16 16:20:40 +0100

    send signal after entry deleted to delete from filesystem

--- src/pages/models.py
+++ src/pages/models.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
 from django.forms import ModelForm
-
+from django.db.models.signals import post_delete
 
 # Create your models here.
 
@@ -13,3 +14,8 @@ class FilesUpload(models.Model):
     file = models.FileField(upload_to="media/")
     type_fichier = models.TextField("Type de fichier", max_length=30, blank=True)
     utilisateur = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, blank=True, null=True)
+
+@receiver(post_delete, sender=FilesUpload)
+def mymodel_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.file.delete(False)
